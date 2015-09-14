9d89a51a046b38c8874b90729741b9e2efb8199d
Adrian Breitburd <adrian.breitburd@globant.com>
2015-09-14 11:41:25 +0530

    initial commit

---
+++ src/main/java/com/globant/poc/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.globant.poc;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Workd");
+
+	}
+
+}
---
+++ src/test/java/gradlePoc/HelloWorld.java
@@ -0,0 +1,14 @@
+package gradlePoc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void testToString() {
+		assertTrue("mmm, something wrong", Boolean.TRUE);
+	}
+
+}
